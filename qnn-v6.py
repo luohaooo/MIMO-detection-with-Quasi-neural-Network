6f64db8aba78899011427155942463d5b1694401
Hao Luo <2926565817@qq.com>
2023-12-09 23:01:07 +0800

    update gradients

--- qnn-v6.py
+++ qnn-v6.py
@@ -11,7 +11,7 @@ from timeit import default_timer as time
 
 
 
-# generate transmit signal
+# generate signals for simulation
 def generate_random_bit_sequence(length):
     return ''.join(random.choice('01') for _ in range(length))
 
@@ -45,7 +45,6 @@ def generate_x_sequence(length, Nt):
 def generate_noise(SNR, Nr):
     return np.sqrt(1/(2*SNR))*(np.random.randn(Nr,1)+1j*np.random.randn(Nr,1))
 
-# generate training and tesing data
 def generate_data(Nr,Nt,SNR_dB,length,H_channel):
     bits_sequence, x_sequence = generate_x_sequence(length, Nt)
     SNR= 10**(SNR_dB/10)
@@ -53,20 +52,31 @@ def generate_data(Nr,Nt,SNR_dB,length,H_channel):
     y_sequence = [np.dot(H_channel, x_sequence[i].reshape(Nt,1)) + n_sequence[i] for i in range(length)]
     return bits_sequence, x_sequence, y_sequence
 
+
+
+# training H_hat
+
+
+
 def bits2signals(bits):
     # bits: input binary string with length of (4*Nt) 
     return np.array([qam16_modulation(bits[i:i+4]) for i in range(0, len(bits), 4)]).reshape(Nt,1)
 
-def calculate_layer1(H_hat, y):
+def calculate_layer1_training(H_hat, y):
     dimension_layer1 = 2**(4*Nt)
+    # layer1 output
     output = np.empty(dimension_layer1)
+    # calculate gradient components in layer1
+    gradients = np.zeros((dimension_layer1, Nr, Nt), dtype=np.complex128)
     for index in range(dimension_layer1):
         bits = str(bin(index)[2:].zfill(4*Nt))
         s = bits2signals(bits)
+        s_conjugate_transpose = s.conj().T
         error = y - np.dot(H_hat,s)
         value =  np.exp(-np.square(np.linalg.norm(error)))
         output[index] = value
-    return output
+        gradients[index] = np.real(-2*np.dot(error, s_conjugate_transpose))+1j*np.imag(2*np.dot(s_conjugate_transpose, s)*H_hat)
+    return output, gradients
 
 def layer2_matrix(n):
     if n == 1:
@@ -74,50 +84,94 @@ def layer2_matrix(n):
     else:
         last_ = layer2_matrix(n-1)
         half_cols_num = 2**(n-1)
-        first_row = np.concatenate((np.zeros(half_cols_num),np.ones(half_cols_num)))
+        first_row = np.concatenate((np.zeros(half_cols_num), np.ones(half_cols_num)))
         remain_rows = np.hstack((last_, last_))
         # print(remain_rows)
         return np.vstack((first_row, remain_rows))
 
 
-def calculate_layer2(layer1_output):
+def calculate_layer2_training(layer1_output, true_output):
     total_prob = np.sum(layer1_output)
-
+    # print(total_prob)
     A = layer2_matrix(4*Nt)
     sum_prob_1 = np.dot(A, layer1_output)
-    
+    # layer2 output
     output = np.array([sum_prob_1[ii]/total_prob for ii in range(4*Nt)])
-
-    return output
+    # calculate gradient components in layer2
+    gradients = np.zeros(2**(4*Nt))
+    for ii in range(len(gradients)):
+        for jj in range(4*Nt):
+            gradients[ii] += 2*(true_output[jj]-output[jj])*(-(A[jj][ii]/total_prob)+(sum_prob_1[jj]/np.square(total_prob)))
+    return output, gradients
 
 
 def calculate_square_error(layer2_output, true_sequence):
-    dimension = len(true_sequence)
-    loss = 0
-    for index in range(dimension):
-        if true_sequence[index] == '1':
-            loss += np.square(1-layer2_output[index])
-        else:
-            loss += np.square(layer2_output[index])
-    return loss
+    return np.linalg.norm(layer2_output-true_sequence)**2
 
-def calculate_cost_function(H_hat_vec):
-    H_hat = H_hat_vec[0:Nr*Nt].reshape(Nr,Nt)+1j*H_hat_vec[Nr*Nt:2*Nr*Nt].reshape(Nr,Nt)
-    # H_hat = H_hat_vec
+
+
+def calculate_cost_function(H_hat):
     total_loss = 0
+    total_gradients = np.zeros((Nr,Nt), dtype=np.complex128)
     training_length = len(y_sequence)
     for ii in range(training_length):
-        layer1_output = calculate_layer1(H_hat, y_sequence[ii])
-        layer2_output = calculate_layer2(layer1_output)
+        # print(ii)
         true_sequence = ''.join(bits_sequence[ii*Nt+jj] for jj in range(Nt))
+        true_sequence = np.array([eval(ii) for ii in true_sequence])
+        layer1_output, layer1_gradients = calculate_layer1_training(H_hat, y_sequence[ii])
+        layer2_output, layer2_gradients = calculate_layer2_training(layer1_output, true_sequence)
         total_loss += calculate_square_error(layer2_output,true_sequence)
+        # SGD
+        # if np.random.rand() < 0.6:
+        for jj in range(2**(4*Nt)):
+            total_gradients += layer2_gradients[jj]*layer1_gradients[jj]
     mean_loss = total_loss/training_length
-    print(mean_loss)
-    return mean_loss
-        
+    return mean_loss, total_gradients
+
+
+def training(max_iter):
+    H_hat = np.sqrt(1/2)*(np.random.randn(Nr,Nt)+1j*np.random.randn(Nr,Nt))
+    # H_hat = H
+    last_gradient = np.zeros((Nr,Nt),dtype=np.complex128)
+    for iter_num in range(max_iter):
+        if iter_num%20 == 0:
+            alpha = eval(input("更新步长："))
+        # solve the gradient
+        mean_loss, total_gradients = calculate_cost_function(H_hat)
+        print(mean_loss)
+        # update H_hat
+        last_gradient = (1-beta)*total_gradients + beta*last_gradient
+        H_hat += alpha * last_gradient
+        # print(H_hat)
+
+    return H_hat
+
+
+# testing QNN for detection
+def calculate_layer1_testing(H_hat, y):
+    dimension_layer1 = 2**(4*Nt)
+    # layer1 output
+    output = np.zeros(dimension_layer1)
+    for index in range(dimension_layer1):
+        bits = str(bin(index)[2:].zfill(4*Nt))
+        s = bits2signals(bits)
+        s_conjugate_transpose = s.conj().T
+        error = y - np.dot(H_hat,s)
+        value =  np.exp(-np.square(np.linalg.norm(error)))
+        output[index] = value
+    return output
+
+def calculate_layer2_testing(layer1_output):
+    total_prob = np.sum(layer1_output)
+    A = layer2_matrix(4*Nt)
+    sum_prob_1 = np.dot(A, layer1_output)
+    # layer2 output
+    output = np.array([sum_prob_1[ii]/total_prob for ii in range(4*Nt)])
+    return output
+
 def detection(y, H_trained):
-    layer1_output = calculate_layer1(H_trained, y)
-    layer2_output = calculate_layer2(layer1_output)
+    layer1_output = calculate_layer1_testing(H_trained, y)
+    layer2_output = calculate_layer2_testing(layer1_output)
     detect_result = ''
     for ii in range(len(layer2_output)):
         if(layer2_output[ii]>0.5):
@@ -129,17 +183,6 @@ def detection(y, H_trained):
 def count_differences(str1, str2):
     return sum(a != b for a, b in zip(str1, str2))
 
-
-def training():
-    H_hat_vec = np.sqrt(1/2)*(np.random.randn(Nr*Nt*2))
-
-    out = minimize(calculate_cost_function, x0=H_hat_vec, method="COBYLA", options={'maxiter':300})
-
-    H_hat_vec = out.x
-
-    H_trained = H_hat_vec[0:Nr*Nt].reshape(Nr,Nt)+1j*H_hat_vec[Nr*Nt:2*Nr*Nt].reshape(Nr,Nt)
-    
-    return H_trained
 
 def calculate_BER(H_trained, bits_sequence_testing, y_sequence_testing):
     error = 0
@@ -156,8 +199,12 @@ Nt = 2
 Nr = 4
 # generate channel
 
-iter_num = 10
-SNR_list = np.array([15])
+iter_num = 1
+SNR_list = np.array([100])
+
+alpha = 1e-8
+beta = 0.1
+
 
 SD_mean_performance = np.zeros(len(SNR_list))
 QNN_mean_performance = np.zeros(len(SNR_list))
@@ -174,13 +221,14 @@ for ii in range(len(SNR_list)):
     for jj in range(iter_num):
         # print("current iter num: " +str(jj))
         H = H_list[jj]
+        # print("真实信道")
         # print(H)
         bits_sequence_testing, x_sequence_testing, y_sequence_testing = generate_data(Nr,Nt,SNR_dB,1024,H)
         SD_performance[jj] = sphere_decoding_BER(H, y_sequence_testing, bits_sequence_testing, 1)
         print("SD: "+str(SD_performance[jj]))
 
         bits_sequence, x_sequence, y_sequence = generate_data(Nr,Nt,SNR_dB,128,H)
-        H_trained = training()
+        H_trained = training(500)
         QNN_performance[jj] = calculate_BER(H_trained, bits_sequence_testing, y_sequence_testing)
         print("QNN: "+str(QNN_performance[jj]))
 
